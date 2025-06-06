0862afd2b8ee9269e9fdf0ebbdc5e201abac4caf
Pedro Nathael Ayres da Silva <106789671+NathaelPedro@users.noreply.github.com>
2025-06-06 12:38:27 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const serverless = require('serverless-http');
+import express from 'express';
+import cors from 'cors';
+import serverless from 'serverless-http';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Rota de teste
 app.get('/', (req, res) => {
   res.json({ message: 'API funcionando na Vercel!' });
 });
 
-// Exporte como handler para Vercel
-module.exports.handler = serverless(app);
+export default serverless(app);
